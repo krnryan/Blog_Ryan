5828979ee52cab39dfb8db525fb231c645675b02
Ryan Choi <ryan.mingyu.choi@gmail.com>
2014-10-22 17:01:29 -0700

    Validation error fixed

--- registration.php
+++ registration.php
@@ -11,14 +11,23 @@
 
     <title>Registration</title>
 	<script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
-
+    <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Custom CSS -->
     <link href="css/clean-blog.min.css" rel="stylesheet">
 
+    <!-- Custom Fonts -->
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 	
 	<style>			
 		#box {
@@ -36,13 +45,14 @@
 			color: red;
 		}
 	</style>
-	
 </head>
 
 <body>
 
+    <!-- Navigation -->
     <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
         <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header page-scroll">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
@@ -53,6 +63,7 @@
                 <a class="navbar-brand" href="index.html">Ryan Mingyu Choi BLOG</a>
             </div>
 
+            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
@@ -69,9 +80,13 @@
                     </li>
                 </ul>
             </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container -->
     </nav>
 
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
     <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
         <div class="container">
             <div class="row">
@@ -86,6 +101,7 @@
         </div>
     </header>
 
+    <!-- Main Content -->
     <div class="container">
 		<div id="message">
 			<?php
@@ -104,10 +120,9 @@
 				}
 			?>
 		</div>
-		
         <div id="box">
 			<form id="reg_form" method="POST">
-				<label for="username">ID</label>
+				<label for="username">Username</label>
 				<input class="form-control" type="text" name="user_id" id="username"/><br/>
 				<label for="userpw">Password</label>
 				<input class="form-control" type="password" name="user_password" id="userpw"/><br/>
@@ -116,12 +131,11 @@
 				<button class="btn btn-primary" type="submit">REGISTER</button>
 			</form>
 		</div>
-		
     </div>
 
     <hr>
 
-
+    <!-- Footer -->
     <footer>
         <div class="container">
             <div class="row">
@@ -157,13 +171,16 @@
             </div>
         </div>
     </footer>
-	
+
+    <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
-    <script src="js/clean-blog.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="js/clean-blog.js"></script>
 	
 	<script>
 		$(function(){
+			// Handle the submit event by validating our fields first
 			$('#reg_form').submit(function(){
 				var patt_username = /^[a-zA-Z0-9]{6,20}$/;
 				var patt_password = /^[a-zA-Z0-9]{6,12}$/;
